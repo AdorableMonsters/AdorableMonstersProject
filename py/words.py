2a3c8fcee474390d29b7ce2bab796fccb82bb795
karencbraga <karencbraga@gmail.com>
2016-06-05 23:19:55 -0300

    Add files via upload

---
+++ py/words.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+global rouge,vert,bleu,noir,jaune,orange,blanc,words
+
+words=[
+       ["Play", "Jogar"], #0
+       ["Exit", "Sair"], #1
+       ["About", "Sobre"], #2       
+       ["Level : ", "Nível : "], #3
+       ["Life: ", "Vida : "], #4
+       ["Bomb: ", "Bomba : "], #5
+       ["Power: ", "Força : "], #6
+       ["Score: ", "Ponto : "], #7
+       ["Force: ","Força: "], #8
+       ["Alien is stronger than you!", "Alien é mais forte que você!"],#9
+       ["Congratulation ... you are more stronger that Alien!", "Parabéns ... você é mais forte que os Aliens!"],#10       
+       ["Adorable Monsters","Adoráveis Monstros"],#11
+       ]
